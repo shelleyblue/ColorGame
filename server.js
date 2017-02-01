5d9f7193889a9fde7a649211caba6025ae991a94
Shelley <shelleyblue@gmail.com>
2017-02-01 21:53:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
+// set the view engine to js
 app.set('view engine', 'js');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 // set the home page route
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.render('colorGame');
 });
 
 app.listen(port, function() {
